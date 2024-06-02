99812dd92687ae1757417bd17203b042f3c46da4
ValeryB97 <valery.babaryko@gmail.com>
2024-06-02 23:14:35 +0700

    Comparison of Hogwarts students.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Griffindor harryPotter = new Griffindor("Гарри Поттер", 43, 42, 53, 65, 89);
-        Griffindor hermioneGranger = new Griffindor("Гермионна Грейнджер", 56, 78, 67, 54, 43);
+        Griffindor hermioneGranger = new Griffindor("Гермионна Грейнджер", 56, 78, 65, 76, 43);
         Griffindor ronaldWeasley = new Griffindor("Рональд Уизли", 53, 35, 65, 76, 43);
         Slytherin dracoMalfoy = new Slytherin("Драко Малфой", 43, 54, 65, 76, 43, 45, 34);
         Slytherin grahamMontague = new Slytherin("Грэхэм Монтегю", 32, 65, 34, 23, 54, 64, 14);
